03dcdaa1921365f5f62091b60091afd4d9aa4227
Adrian Holfter <git@adrian.holfter.de>
2018-08-02 17:34:51 +0200

    SingleColumnTypeIterator

--- src/lib/storage/reference_column/reference_column_iterable.hpp
+++ src/lib/storage/reference_column/reference_column_iterable.hpp
@@ -25,9 +25,25 @@ class ReferenceColumnIterable : public ColumnIterable<ReferenceColumnIterable<T>
     const auto begin_it = _column.pos_list()->begin();
     const auto end_it = _column.pos_list()->end();
 
-    auto begin = Iterator{table, column_id, begin_it, begin_it};
-    auto end = Iterator{table, column_id, begin_it, end_it};
-    functor(begin, end);
+    if(_column.pos_list()->empty()) {
+      return;
+    }
+
+    // resolve first column type
+    const auto first_chunk = _column.referenced_table()->get_chunk(_column.pos_list()->cbegin()->chunk_id);
+    const auto first_column = first_chunk->get_column(_column.referenced_column_id());
+    resolve_column_type<T>(*first_column, [&](const auto& typed_column){
+      using ConcreteColumnType = std::decay_t<decltype(typed_column)>;
+      constexpr auto is_reference_column = std::is_same_v<ConcreteColumnType, ReferenceColumn>;
+
+      if constexpr(!is_reference_column) {
+        auto begin = SingleColumnTypeIterator<ConcreteColumnType>{table, column_id, begin_it, begin_it};
+        auto end = SingleColumnTypeIterator<ConcreteColumnType>{table, column_id, begin_it, end_it};
+        functor(begin, end);
+      } else {
+        Fail("Reference column must not reference another reference column");
+      }
+    });
   }
 
  private:
@@ -90,6 +106,65 @@ class ReferenceColumnIterable : public ColumnIterable<ReferenceColumnIterable<T>
     PosListIterator _pos_list_it;
 
     std::vector<std::shared_ptr<const BaseTypedColumn<T>>> _columns;
+  };
+
+  template <typename ConcreteColumnType>
+  class SingleColumnTypeIterator : public BaseColumnIterator<SingleColumnTypeIterator<ConcreteColumnType>, ColumnIteratorValue<T>> {
+   public:
+    using PosListIterator = PosList::const_iterator;
+
+   public:
+    explicit SingleColumnTypeIterator(const std::shared_ptr<const Table> table, const ColumnID column_id,
+                      const PosListIterator& begin_pos_list_it, const PosListIterator& pos_list_it)
+        : _table{table},
+          _column_id{column_id},
+          _begin_pos_list_it{begin_pos_list_it},
+          _pos_list_it{pos_list_it},
+          _columns{_table->chunk_count(), nullptr} {
+      for (auto chunk_id = ChunkID{0}; chunk_id < _table->chunk_count(); ++chunk_id) {
+        _insert_referenced_column(chunk_id);
+      }
+    }
+
+   private:
+    friend class boost::iterator_core_access;  // grants the boost::iterator_facade access to the private interface
+
+    void increment() { ++_pos_list_it; }
+
+    bool equal(const SingleColumnTypeIterator& other) const { return _pos_list_it == other._pos_list_it; }
+
+    ColumnIteratorValue<T> dereference() const {
+      if (_pos_list_it->is_null()) return ColumnIteratorValue<T>{T{}, true, 0u};
+
+      const auto chunk_id = _pos_list_it->chunk_id;
+      const auto& chunk_offset = _pos_list_it->chunk_offset;
+
+      const auto chunk_offset_into_ref_column =
+          static_cast<ChunkOffset>(std::distance(_begin_pos_list_it, _pos_list_it));
+
+      const auto typed_value = _columns[chunk_id]->get_typed_value(chunk_offset);
+
+      return ColumnIteratorValue<T>{typed_value.value_or(T{}), !typed_value.has_value(), chunk_offset_into_ref_column};
+    }
+
+   private:
+    void _insert_referenced_column(const ChunkID chunk_id) {
+      if (_columns[chunk_id] == nullptr) {
+        auto column = _table->get_chunk(chunk_id)->get_column(_column_id);
+        auto concrete_column = std::dynamic_pointer_cast<const ConcreteColumnType>(column);
+        DebugAssert(concrete_column, "Cannot cast to concrete column type");
+        _columns[chunk_id] = concrete_column;
+      }
+    }
+
+   private:
+    const std::shared_ptr<const Table> _table;
+    const ColumnID _column_id;
+
+    const PosListIterator _begin_pos_list_it;
+    PosListIterator _pos_list_it;
+
+    std::vector<std::shared_ptr<const ConcreteColumnType>> _columns;
   };
 };
 
