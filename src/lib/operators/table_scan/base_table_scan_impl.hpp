67e5278294ed0c4dcb8359e7420c9289c3c176b1
Markus Dreseler <markus.dreseler@hpi.de>
2018-08-17 19:18:26 +0200

    TableScan: Predication for unary_scan_with_value

--- src/lib/operators/table_scan/base_table_scan_impl.hpp
+++ src/lib/operators/table_scan/base_table_scan_impl.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <functional>
 #include <memory>
 
@@ -30,6 +31,47 @@ class BaseTableScanImpl {
    * @{
    */
 
+  // Version with a constant value on the right side. Sometimes we prefer this over _unary_scan because we can use
+  // with_comparator.
+  template <typename BinaryFunctor, typename LeftIterator, typename RightValue>
+  void __attribute__((noinline))
+  _unary_scan_with_value(const BinaryFunctor& func, LeftIterator left_it, LeftIterator left_end, RightValue right_value,
+                         const ChunkID chunk_id, PosList& matches_out) {
+    // branchless scan, based on https://stackoverflow.com/a/38799396/2204581
+    constexpr auto BUFFER_SIZE = 512l;
+    std::array<ChunkOffset, BUFFER_SIZE> buffer;
+
+    // In the beginning, we do blocks of BUFFER_SIZE values - this should allow the compiler to unroll the loop
+    while (left_end - left_it > BUFFER_SIZE) {
+      auto insert_index = 0;
+
+      for (auto i = size_t{0}; i < BUFFER_SIZE; ++i, ++left_it) {
+        const auto left = *left_it;
+
+        buffer[insert_index] = left.chunk_offset();
+
+        // This is where the "check" happens. If the value is not null and the condition is met, the insert_index is
+        // moved forward. Otherwise, it remains where it is and the value that was written before will be overwritten.
+        // Since this is not a condition, we don't introduce a branch.
+        insert_index += (!left.is_null() & func(left.value(), right_value));
+      }
+
+      for (auto i = 0; i < insert_index; ++i) {
+        matches_out.emplace_back(RowID{chunk_id, buffer[i]});
+      }
+
+      insert_index = 0;
+    }
+
+    // Do the remainder
+    for (; left_it != left_end; ++left_it) {
+      const auto left = *left_it;
+      if (!left.is_null() & func(left.value(), right_value)) {
+        matches_out.emplace_back(RowID{chunk_id, left.chunk_offset()});
+      }
+    }
+  }
+
   template <typename UnaryFunctor, typename LeftIterator>
   // noinline reduces compile time drastically
   void __attribute__((noinline)) _unary_scan(const UnaryFunctor& func, LeftIterator left_it, LeftIterator left_end,
@@ -40,23 +82,6 @@ class BaseTableScanImpl {
       if (left.is_null()) continue;
 
       if (func(left.value())) {
-        matches_out.push_back(RowID{chunk_id, left.chunk_offset()});
-      }
-    }
-  }
-
-  // Version with a constant value on the right side. Sometimes we prefer this over _unary_scan because we can use
-  // with_comparator.
-  template <typename BinaryFunctor, typename LeftIterator, typename RightValue>
-  void __attribute__((noinline))
-  _unary_scan_with_value(const BinaryFunctor& func, LeftIterator left_it, LeftIterator left_end, RightValue right_value,
-                         const ChunkID chunk_id, PosList& matches_out) {
-    for (; left_it != left_end; ++left_it) {
-      const auto left = *left_it;
-
-      if (left.is_null()) continue;
-
-      if (func(left.value(), right_value)) {
         matches_out.push_back(RowID{chunk_id, left.chunk_offset()});
       }
     }
